46e282504cf6794178b248207e0497279240fa0c
unknown <qpdevil83@gmail.com>
2024-04-12 17:19:00 +0430

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div className="bg-pirmary text-primary ">
+      App
+
+
+
+      {/* attribution */}
+      <div class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="/">Your Name Here</a>.
+      </div>
+    </div>
+  );
+}
+
+export default App;
