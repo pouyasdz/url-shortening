c96600b7a43af64d98eba85fd45a1e6b17505082
unknown <qpdevil83@gmail.com>
2024-04-13 22:52:10 +0430

    complete responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,27 @@ function App() {
     <>
       <Header />
       <main className="overflow-y-auto">
-        <img
-          src="/images/illustration-working.svg"
-          alt=""
-          className="landing"
-        />
-
-        <div className="cta w-100 h-25 d-flex align-items-center  flex-column">
-          <h1 className="text-center fw-bolder">
-            More than just shorter links
-          </h1>
-          <p>
-            Build Your brands recognition and get detailed insights on how your
-            link are performing
-          </p>
-          <button className="btn-primary btn-rounded">Get Started</button>
+        <div className="d-flex flex-column flex-md-row-reverse  align-items-center ">
+          <img
+            src="/images/illustration-working.svg"
+            alt=""
+            className="landing"
+          />
+          <div className="cta w-100 h-25 d-flex align-items-center flex-column align-items-md-start ">
+            <h1 className="text-center text-md-start fw-bolder">
+              More than just shorter links
+            </h1>
+            <p className="text-center text-md-start">
+              Build Your brands recognition and get detailed insights on how
+              your link are performing
+            </p>
+            <button className="btn-primary btn-rounded">Get Started</button>
+          </div>
         </div>
-
-        <form className="input-form">
-          <input type="text" placeholder="Shorten a link here..." />
-          <button className="btn-primary">Shorten it!</button>
+        <form 
+        className="input-form d-flex flex-column justify-content-center align-items-center flex-md-row p-3 gap-3 ps-md-5 pe-md-5">
+          <input className="w-100 h-50" type="text" placeholder="Shorten a link here..." />
+          <button className="btn-primary h-50 w-100">Shorten it!</button>
         </form>
 
         <div className="advance d-flex flex-column  gap-2 mt-5 ">
@@ -37,8 +38,8 @@ function App() {
           </p>
         </div>
 
-        <div className="d-grid mt-5 brand">
-          <div className="d-flex flex-column gap-2 ">
+        <div className="d-flex flex-column  mt-5 brand align-items-center flex-lg-row">
+          <div className="d-flex flex-column gap-2 align-self-lg-baseline ">
             <div className="bg">
               <img src="/images/icon-brand-recognition.svg" alt="" />
             </div>
@@ -49,6 +50,7 @@ function App() {
               content.
             </p>
           </div>
+          <div className="col-5 line align-self-lg-baseline "></div>
           <div className="d-flex flex-column gap-2 ">
             <div className="bg">
               <img src="/images/icon-detailed-records.svg" alt="" />
@@ -60,7 +62,9 @@ function App() {
               decisions.
             </p>
           </div>
-          <div className="d-flex flex-column gap-2 ">
+
+          <div className="col-5 line align-self-lg-baseline"></div>
+          <div className="d-flex flex-column gap-2 align-self-lg-end ">
             <div className="bg">
               <img src="/images/icon-fully-customizable.svg" alt="" />
             </div>
@@ -71,16 +75,8 @@ function App() {
             </p>
           </div>
         </div>
-        {/* attribution */}
-        {/* <div className="attribution position-absolute  bottom-0  fs-6">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="/">Pouya sadeghzadeh</a>.
-      </div> */}
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,60 +3,75 @@ function Footer() {
   return (
     <footer className="w-100 d-flex  flex-column align-items-center pb-5">
       <div className="boost">
-        <h2 className="fw-bold">Boost your link today</h2>
+        <h2 className="fw-bold">Boost your links today</h2>
         <a href="#boost" className="btn-primary btn-rounded">
           Get started
         </a>
       </div>
-      <h2 className="mt-5 fw-bold fs-1">Shortly</h2>
-      <div className="d-grid mt-5 gap-5 shortly">
-        <ul className="d-flex flex-column gap-2 align-items-center p-0">
-          <span className="fs-4 fw-medium ">Features</span>
-          <li>
-            <a href="#blank">Link Shortening</a>
-          </li>
-          <li>
-            <a href="#blank">Branded Links</a>
-          </li>
-          <li>
-            <a href="#blank">Analytics</a>
-          </li>
-        </ul>
 
-        <ul className="d-flex flex-column gap-2 align-items-center p-0">
-          <span className="fs-4 fw-medium ">Resources</span>
-          <li>
-            <a href="#blank">Blog</a>
-          </li>
-          <li>
-            <a href="#blank">Developers</a>
-          </li>
-          <li>
-            <a href="#blank">Support</a>
-          </li>
-        </ul>
+      <div className="w-100 ps-md-5 pe-md-5 d-flex  flex-column align-items-center flex-md-row align-items-md-start justify-content-md-between">
+        
+        <h2 className="mt-5 fw-bold fs-1">Shortly</h2>
+        <div className="d-grid mt-5 gap-5 shortly">
+          <ul className="d-flex flex-column gap-2 align-items-center p-0">
+            <span className="fs-4 fw-medium ">Features</span>
+            <li>
+              <a href="#blank">Link Shortening</a>
+            </li>
+            <li>
+              <a href="#blank">Branded Links</a>
+            </li>
+            <li>
+              <a href="#blank">Analytics</a>
+            </li>
+          </ul>
 
-        <ul className="d-flex flex-column gap-2 align-items-center p-0">
-          <span className="fs-4 fw-medium ">Company</span>
-          <li>
-            <a href="#blank">About</a>
-          </li>
-          <li>
-            <a href="#blank">Our Team</a>
-          </li>
-          <li>
-            <a href="#blank">Careers</a>
-          </li>
-          <li>
-            <a href="#blank">Contact</a>
-          </li>
-        </ul>
+          <ul className="d-flex flex-column gap-2 align-items-center p-0">
+            <span className="fs-4 fw-medium ">Resources</span>
+            <li>
+              <a href="#blank">Blog</a>
+            </li>
+            <li>
+              <a href="#blank">Developers</a>
+            </li>
+            <li>
+              <a href="#blank">Support</a>
+            </li>
+          </ul>
+
+          <ul className="d-flex flex-column gap-2 align-items-center p-0">
+            <span className="fs-4 fw-medium ">Company</span>
+            <li>
+              <a href="#blank">About</a>
+            </li>
+            <li>
+              <a href="#blank">Our Team</a>
+            </li>
+            <li>
+              <a href="#blank">Careers</a>
+            </li>
+            <li>
+              <a href="#blank">Contact</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="d-flex text-white mt-5 gap-4 fs-1 ">
+          <i className="ri-facebook-box-fill"></i>
+          <i className="ri-twitter-fill"></i>
+          <i className="ri-pinterest-fill"></i>
+          <i className="ri-instagram-line"></i>
+        </div>
+
       </div>
-      <div className="d-flex text-white mt-5 gap-4 fs-1 ">
-        <i class="ri-facebook-box-fill"></i>
-        <i class="ri-twitter-fill"></i>
-        <i class="ri-pinterest-fill"></i>
-        <i class="ri-instagram-line"></i>
+
+      {/* attribution */}
+      <div className="text-center ">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="/">Pouya sadeghzadeh</a>.
       </div>
     </footer>
   );
